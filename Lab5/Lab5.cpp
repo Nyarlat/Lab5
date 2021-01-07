0bdbbe4c4bbce7407f0550781ca81d090384cc1f
Nyarlat <nyarlat7@gmail.com>
2021-01-07 16:32:20 +0500

    Добавил новый класс Toucan

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 
 class  Bird {
-protected:
-    string classname;
 public:
     Bird() {
         printf("Bird()\n");
@@ -20,12 +18,16 @@ public:
     }
     virtual ~Bird() {
         printf("~Bird()\n");
-        printf("\n");
     }
 
-    virtual string ClassName() {
-        printf("ClassName is Bird\n");
+    virtual string GetClassName() {
         return "Bird";
+    }
+
+    virtual bool isA(string classname) {
+        if (classname == "Bird")
+            return true;
+        return false;
     }
 };
 
@@ -37,32 +39,68 @@ public:
     }
     Goose(const Goose* g) {
         printf("Goose(const Goose* g)\n");
-
     }
 
     Goose(const Goose& g) {
         printf("Goose(const Goose& g)\n");
-
     }
 
     ~Goose() {
         printf("~Goose()\n");
     }
 
-    string ClassName() {
-        printf("ClassName is Goose\n");
+    string GetClassName() {
         return "Goose";
+    }
+
+    bool isA(string classname) {
+        if (classname == "Goose")
+            return true;
+        return false;
+    }
+};
+
+class Toucan : public Bird {
+public:
+    Toucan() :Bird() {
+        printf("Toucan()\n");
+    }
+    Toucan(const Toucan* t) {
+        printf("Toucan(const Toucan* g)\n");
+    }
+
+    Toucan(const Toucan& t) {
+        printf("Toucan(const Toucan& g)\n");
+    }
+
+    ~Toucan() {
+        printf("~Toucan()\n");
+    }
+
+    string GetClassName() {
+        return "Toucan";
+    }
+
+    bool isA(string classname) {
+        if (classname == "Toucan")
+            return true;
+        return false;
     }
 };
 
 
 int main()
 {
-    Bird *b1 = new Bird;
-    b1->ClassName();
-    Goose* g1 = new Goose;
-    g1->ClassName();
-    Bird* g2 = new Goose;
-    g2->ClassName();
+
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
